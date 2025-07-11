cf5458c83d3d12fab2f0a59255d00fe37196c782
Nguyễn Lê Ngọc Duy <ngocdilearnds@gmail.com>
2025-07-11 21:08:28 +0700

    [UPDATE] Implement Bisection method

---
+++ bisection_method/matlab_script.m
@@ -0,0 +1,42 @@
+clear all;
+close all;
+clc;
+
+function x = bisection_method(f, a, b, max_iters, tol)
+    f_a = f(a);
+    if f_a < tol
+        x = a;
+        return;
+    end
+    f_b = f(b);
+    if f_b < tol
+        x = b;
+        return;
+    end
+
+    if f_a * f_b > 0
+        disp("f(a) and f(b) must have different signs")
+        return;
+    end
+    
+    for iter = 1:max_iters
+        mid = (a + b) / 2;
+        if abs(b - a) < tol
+            break;
+        end
+        if abs(f_mid) < tol
+            break;
+        end
+        f_mid = f(mid);
+        if abs(f_c) < tol
+            break;
+        elseif f_a * f_mid < 0
+            b = mid;
+            f_b = f_mid;
+        else
+            a = mid;
+            f_a = f_mid;
+        end
+    end
+    x = mid;
+end
---
+++ bisection_method/python_script.py
@@ -0,0 +1,31 @@
+import math
+from collections.abc import Callable
+
+EPSILON = 1e-12
+
+
+def bisection_algorithm(f: Callable[[float], float], a: float, b: float, num_iters: int=100, tol: float=EPSILON) -> float | None:
+    f_a = f(a)
+    if abs(f_a) < tol: return a
+
+    f_b = f(b)
+    if abs(f_b) < tol: return b
+
+    # Check if f_a and f_b have the same signs
+    if f_a * f_b > 0:
+        print("f(a) and f(b) must have different signs")
+        return None
+
+    for _ in range(num_iters):
+        mid_point = (a + b) / 2
+        if abs(b - a) < tol: break
+        f_mid = f(mid_point)
+        if abs(f_mid) < tol: break
+        if f_a * f_mid > 0:
+            a = mid_point
+            f_a = f_mid
+        if f_b * f_mid > 0:
+            b = mid_point
+            f_b = f_mid
+
+    return mid_point
